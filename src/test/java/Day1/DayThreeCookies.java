fdd5a057ba1ff9352d75a028479a24fff6c236c6
Sas5999 <Sahilsync7900@gmail.com>
2023-11-23 18:43:12 +0530

    Added dynamic query and path params

--- src/test/java/Day1/DayThreeCookies.java
+++ src/test/java/Day1/DayThreeCookies.java
@@ -1,4 +1,33 @@
 package Day1;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
 public class DayThreeCookies {
-}
+
+
+    @Test()
+    void GetUser() {
+        given()
+                .pathParam("type","unknown") //Using dynamic variables.
+                .pathParam("id",2)
+
+                .when().get("https://reqres.in/api/{type}/{id}")
+                .then().statusCode(200)
+                .log().all()
+                .body("data.id",equalTo(2))
+                .body("data.name",equalTo("fuchsia rose"));
+    }
+    @Test()
+    void GetUsers(){
+        given()// query params are added
+                .pathParam("user_type","users")
+                .queryParam("page", 2)
+                .when().get("https://reqres.in/api/{user_type}")
+                .then().statusCode(200).log().all();
+    }
+    }
+
